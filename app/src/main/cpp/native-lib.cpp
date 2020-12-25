99d30f5674301c9554e27ae93b385b0549f129bf
Andrew <andriy.serb1@gmail.com>
2020-12-25 21:48:05 +0100

    Audio decoding without ffmpeg code

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -166,3 +166,170 @@ Java_linc_com_amplituda_Amplituda_amplitudesFromAudioJNI(
     avformat_close_input(&formatContext);
     return env->NewStringUTF(resultFrame.data());
 }
+
+
+/*
+
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+using std::string;
+using std::fstream;
+
+typedef struct  WAV_HEADER
+{
+
+uint8_t             RIFF[4];        // RIFF Header      Magic header
+uint32_t            ChunkSize;      // RIFF Chunk Size
+uint8_t             WAVE[4];        // WAVE Header
+
+uint8_t             fmt[4];         // FMT header
+uint32_t            Subchunk1Size;  // Size of the fmt chunk
+uint16_t            AudioFormat;    // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+uint16_t            NumOfChan;      // Number of channels 1=Mono 2=Sterio
+uint32_t             SamplesPerSec;  // Sampling Frequency in Hz
+uint32_t             bytesPerSec;    // bytes per second
+uint16_t             blockAlign;     // 2=16-bit mono, 4=16-bit stereo
+uint16_t             bitsPerSample;  // Number of bits per sample
+
+uint8_t              Subchunk2ID[4]; // "data"  string
+uint32_t             Subchunk2Size;  // Sampled data length
+
+}wav_hdr;
+
+
+int getFileSize(FILE* inFile);
+float getSample(uint16_t bps, uint8_t* buffer, int sampleIndex);
+
+int main(int argc, char* argv[])
+{
+
+    FILE *amps = fopen("/home/linc/Музика/amps_.txt", "a+");
+
+    wav_hdr wavHeader;
+    int headerSize = sizeof(wav_hdr), filelength = 0;
+
+    const char* filePath;
+    string input;
+    if (argc <= 1)
+    {
+        cout << "Input wave file name: /home/linc/Музика/kygo.mp3";
+//        cin >> input;
+//        cin.get();
+        filePath = "/home/linc/Музика/kygo.wav";
+    }
+    else
+    {
+        filePath = argv[1];
+        cout << "Input wave file name: " << filePath << endl;
+    }
+
+    FILE* wavFile = fopen(filePath, "r");
+    if (wavFile == nullptr)
+    {
+        fprintf(stderr, "Unable to open wave file: %s\n", filePath);
+        return 1;
+    }
+
+//Read the header
+    size_t bytesRead = fread(&wavHeader, 1, headerSize, wavFile);
+    cout << "Header Read " << bytesRead << " bytes." << endl;
+    if (bytesRead > 0)
+    {
+        //Read the data
+        uint16_t bytesPerSample = wavHeader.bitsPerSample / 8;      //Number     of bytes per sample
+        uint64_t numSamples = wavHeader.ChunkSize / bytesPerSample; //How many samples are in the wav file?
+        static const uint16_t BUFFER_SIZE = 4096;
+        uint8_t* buffer = new uint8_t[BUFFER_SIZE];
+
+        int s_index = 0;
+
+        while ((bytesRead = fread(buffer, sizeof buffer[0], BUFFER_SIZE / (sizeof buffer[0]), wavFile)) > 0)
+        {
+
+            float sample = getSample(bytesPerSample, buffer, s_index);
+
+            if(sample < 0)
+                sample = -sample;
+
+
+            int amplitude = static_cast<int>(pow(((int)(((sample * 2) / 4) * 100)), 0.5));
+            fprintf(amps, "%d\n", amplitude);
+
+            cout << "Read " << amplitude << " bytes." << endl;
+
+            s_index++;
+        }
+        delete [] buffer;
+        buffer = nullptr;
+        filelength = getFileSize(wavFile);
+
+        cout << "File is :" << filelength << " bytes." << endl;
+        cout << "RIFF header :" << wavHeader.RIFF[0] << wavHeader.RIFF[1] << wavHeader.RIFF[2] << wavHeader.RIFF[3] << endl;
+        cout << "WAVE header :" << wavHeader.WAVE[0] << wavHeader.WAVE[1] << wavHeader.WAVE[2] << wavHeader.WAVE[3] << endl;
+        cout << "FMT :" << wavHeader.fmt[0] << wavHeader.fmt[1] << wavHeader.fmt[2] << wavHeader.fmt[3] << endl;
+        cout << "Data size :" << wavHeader.ChunkSize << endl;
+
+        // Display the sampling Rate from the header
+        cout << "Sampling Rate :" << wavHeader.SamplesPerSec << endl;
+        cout << "Number of bits used :" << wavHeader.bitsPerSample << endl;
+        cout << "Number of channels :" << wavHeader.NumOfChan << endl;
+        cout << "Number of bytes per second :" << wavHeader.bytesPerSec << endl;
+        cout << "Data length :" << wavHeader.Subchunk2Size << endl;
+        cout << "Audio Format :" << wavHeader.AudioFormat << endl;
+        // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+
+        cout << "Block align :" << wavHeader.blockAlign << endl;
+        cout << "Data string :" << wavHeader.Subchunk2ID[0] << wavHeader.Subchunk2ID[1] << wavHeader.Subchunk2ID[2] << wavHeader.Subchunk2ID[3] << endl;
+
+
+    }
+    fclose(wavFile);
+    fclose(amps);
+
+    return 0;
+}
+
+// find the file size
+int getFileSize(FILE* inFile)
+{
+    int fileSize = 0;
+    fseek(inFile, 0, SEEK_END);
+
+    fileSize = ftell(inFile);
+
+    fseek(inFile, 0, SEEK_SET);
+    return fileSize;
+}
+
+float getSample(uint16_t bps, uint8_t* buffer, int sampleIndex) {
+    int64_t val = 0;
+    float ret = 0;
+    int sampleSize = bps;
+
+    switch(sampleSize) {
+        case 1:
+            val = (reinterpret_cast<uint8_t*>(buffer))[sampleIndex];
+            val -= 127;
+            break;
+        case 2: val = (reinterpret_cast<uint16_t*>(buffer))[sampleIndex];
+            break;
+        case 4: val = (reinterpret_cast<uint32_t*>(buffer))[sampleIndex];
+            break;
+        case 8: val = (reinterpret_cast<uint64_t*>(buffer))[sampleIndex];
+            break;
+        default:
+            return 0;
+    }
+
+    ret = val / (static_cast<float>(((1 << (sampleSize*8-1))-1)));
+
+    return ret;
+
+}
+
+ */
